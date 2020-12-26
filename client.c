eb98c757ffecdd1dbc522ef15152ff8047ae9968
ffashion <1323579954@qq.com>
2020-12-26 20:18:28 +0800

    fix bug ; complete file server

--- client.c
+++ client.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <stdint.h>
 #include "client.h"
+
 int main(int argc,char *args[]){
     FILE *fp = NULL;
     
@@ -18,31 +19,71 @@ int main(int argc,char *args[]){
     int client_fd = socket(AF_INET,SOCK_STREAM,0);
     
     struct package receive_package = {0};
-    receive_package.filename = malloc(100);
-    
+    receive_package.filename = malloc(1024);
+
+
     if(connect(client_fd,(struct sockaddr *)&client,sizeof(client)) == -1){
         printf("连接服务器失败");
     }
     
 
     read(client_fd,&receive_package.package_len,4);
-    printf("%d\n",receive_package.package_len);
+    printf("package len:%d\n",receive_package.package_len);
     
     read(client_fd,&receive_package.filename_len,4);
-    printf("%d\n",receive_package.filename_len);
+    printf("filename len:%d\n",receive_package.filename_len);
     
     read(client_fd,&receive_package.file_content_len,4);
-    printf("%d\n",receive_package.file_content_len);
+    printf("file content len:%d\n",receive_package.file_content_len);
     
     read(client_fd,receive_package.filename,receive_package.filename_len);
-    printf("%s\n",receive_package.filename);
+    printf("filename :%s\n",receive_package.filename);
 
-    read(client_fd,receive_package.file_content,receive_package.file_content_len);
-    
-    fp = fopen(receive_package.filename,"w+");
+    char *new_filename = malloc(100);
+    strcat(new_filename,"new_");
+    strcat(new_filename,receive_package.filename);
+    fp = fopen(new_filename,"wb+");
+
     receive_package.file_content = malloc(receive_package.file_content_len);
+
+    //记录已读如内存的2048个数
+    //int i = 0;
     
-    fwrite(receive_package.file_content,1,receive_package.file_content_len,fp);
+    int get_file_content_section_num = receive_package.file_content_len / 2048; //以2048分片
+    int Last_bytes = receive_package.file_content_len % 2048; 
+    
+    for(int i=0;i<=get_file_content_section_num -1;i++){
+        if(read(client_fd,(uint8_t *)receive_package.file_content + 2048*i,2048) == -1){
+            printf("read error");
+        }
+    }
+
+    if(read(client_fd,(uint8_t *)receive_package.file_content + 2048*get_file_content_section_num,Last_bytes) == -1){
+            printf("read error");
+    }
+    
+    // while(1){
+    //     if(receive_package.file_content_len )
+
+    //     if(read(client_fd,(uint8_t *)receive_package.file_content + 2048*i,2048) == -1){
+            
+    //         read(client_fd,(uint8_t *)receive_package.file_content + 2048*i,receive_package.file_content_len-2048*i);
+    //         i++;
+    //         //exit(-1);
+    //     }else{
+    //         i++;
+    //     }
+
+    //     if(2048*i >= receive_package.file_content_len){
+    //         break;
+    //     }
+        
+    // }
+    
+    if(fwrite(receive_package.file_content,1,receive_package.file_content_len,fp) == -1){
+        printf("fwrite error");
+    }
+    
 
     close(client_fd);
     free(receive_package.filename);
--- server.c
+++ server.c
@@ -20,13 +20,14 @@ int server_listen(){
     }
     printf("socket create ok\n");
     if(bind(server_fd,(struct sockaddr *)&server,sizeof(server)) == -1){
-        printf("socket bind failue\n");
+        printf("socket bind error\n");
         exit(-1);
     }
     printf("socket bind ok\n");
     
     if(listen(server_fd,4) == -1){
         printf("listen error\n");
+        exit(-1);
     }
     printf("listen ok\n");
     return server_fd;
@@ -40,6 +41,7 @@ int accept_request(int server_fd){
     int client_fd = accept(server_fd,(struct sockaddr*)&tmp_sock,&tmp_sock_len);
     if(client_fd == -1){
         printf("accept error\n");
+        exit(-1);
     }
     printf("accept ok\n");
     return client_fd;
@@ -47,62 +49,87 @@ int accept_request(int server_fd){
 long get_file_size(char *filename){
     FILE *fp = NULL;
     
-    if((fp = fopen(filename,"r")) == NULL){
+    if((fp = fopen(filename,"rb")) == NULL){
         printf("获取文件失败");
         return -1;
     }
     fseek(fp,0,SEEK_END);
     //获取文件大小
     long file_size = ftell(fp);
-    
     fclose(fp);
     return file_size;
 }
 char  *set_file2buf(char *filename){
     FILE *fp = NULL;
-    char *buffer = NULL;
-    if((fp = fopen(filename,"r")) == NULL){
-        printf("获取文件失败");
-        return NULL;
+    long file_size;
+    char *file_buf;
+    if((fp = fopen(filename,"rb")) == NULL){
+        printf("文件打开失败\n");
+        exit(-1);
     }
-    fseek(fp,0,SEEK_END);
-    //获取文件大小
-    long file_size = ftell(fp);
-    buffer = malloc(file_size);
-    memset(buffer,0,file_size);
-    rewind(fp);
-    fread(buffer,1,file_size,fp);
+    file_size = get_file_size(filename);
+    file_buf = malloc(file_size);
+    
+    memset(file_buf,0,file_size);
+    rewind(fp);//设置当前的读取偏移位置为文件开头
+    fread(file_buf,1,file_size,fp);
+    
     fclose(fp);
-    return buffer;
+    return file_buf;
 
 }
 
 int main(int argc,char *args[]){
      char *filename = NULL;
     if(argc >= 2){
-        filename = "test.txt";
+        filename = args[1];
     }else{
         printf("请指定要传输的文件\n");
         exit(-1);
     }
+    
     int server_fd = server_listen();
     int client_fd = -1;
     struct package package = {0};
+    long file_size = get_file_size(filename);
+    int def_sock_buf_size = -1;
+    socklen_t opt_len;
+    
     //pthread_create();
     while(1){
         client_fd= accept_request(server_fd);
+        getsockopt(client_fd,SOL_SOCKET, SO_SNDBUF,&def_sock_buf_size,&opt_len);
         
-        package.package_len = 4 + strlen(filename)+1 + get_file_size(filename);
+        
+        package.package_len = 4 + 4 + 4 + strlen(filename)+1 + file_size;
         package.filename_len = strlen(filename) +1;
-        package.file_content_len = get_file_size(filename);
+        package.file_content_len = file_size;
         package.filename = filename;
         package.file_content = set_file2buf(filename);
         
+
         write(client_fd,&package.package_len,4);
         write(client_fd,&package.filename_len,4);
         write(client_fd,&package.file_content_len,4);
         write(client_fd,package.filename,strlen(filename)+1);
-        write(client_fd,package.file_content,get_file_size(filename));
+        
+        int get_file_content_section_num = package.file_content_len / 2048; //以2048分片
+        int Last_bytes = package.file_content_len % 2048; 
+        
+        for(int i=0;i<=get_file_content_section_num -1;i++){
+            if(write(client_fd,(uint8_t *)package.file_content + 2048*i,2048) == -1){
+                printf("write error");
+            }
+        }
+
+        if(write(client_fd,(uint8_t *)package.file_content + 2048*get_file_content_section_num,Last_bytes) == -1){
+                printf("write error");
+        }
+        //for test 说明内存内容每次,socket 套接字传输有问题
+        // FILE * fp_test = NULL;
+        // fp_test = fopen("what.mp4","wb+");;
+        // fwrite(package.file_content,1,package.file_content_len,fp_test);
+        
         close(client_fd);
     }
     
